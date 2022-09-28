7f7351f47b620b3bbf0feeeb97aa613d537c7d9f
Brian Lambert <brianlambert2408@gmail.com>
2022-09-29 09:15:54 +1000

    commit tree 3e2086ec716c062557ecff3a8b26efb394cc1873

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
+
   //add static methods here
+  static capitalize(s){
+    return `${s[0].toUpperCase()}${s.slice(1)}`;
+  }
+
+  static sanitize(s){
+    return s.replace(/[^-' A-Za-z0-9]+/g,"");
+  }
+  static titleize(s){
+    const excluded = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const split = s.split(" ");
+
+    split[0] = this.capitalize(split[0]);
+    
+    return split.map(word=>{
+      if(excluded.filter(ex=>ex === word).length <= 0){
+        return this.capitalize(word);
+      }else{
+        return word;
+      }
+    }).join(" ");
+  }
 }
